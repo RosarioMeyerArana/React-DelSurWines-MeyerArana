c6c29b095b31744a213e515fee039aec5b4fbaab
RosarioMeyerArana <rosario.meyera@gmail.com>
2021-04-15 14:50:18 +0200

    cart widget

---
+++ my-app/src/Components/CartWidget/CartWidget.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShoppingCartRoundedIcon from '@material-ui/icons/ShoppingCartRounded';
+
+const CartWidget = () => {
+
+    return(
+        <ShoppingCartRoundedIcon/>
+    )
+
+}
+
+export default CartWidget
---
+++ my-app/src/Components/NavBar/NavBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import CartWidget from '../CartWidget/CartWidget';
+import css from '../NavBar/NavBar.css';
+import Button from 'react-bootstrap/Button';
+import logo from '../../Images/logo.png';
+
+const NavBar = () => {
+
+    return(
+        <header>
+        <nav className="navbar">
+            <div className="container-logo">
+            <img src="{logo}" alt=""/>
+            </div>
+
+            <ul className="navbar-item-container">
+                <li className="navbar-item">Nosotros</li>
+                <li className="navbar-item">Bodegas</li>
+                <li className="navbar-item">Contacto</li>
+                <li className="navbar-item">Shop</li>
+            </ul>
+
+            <CartWidget/>
+
+        </nav>
+        </header>
+    )
+    
+
+}
+
+export default NavBar
