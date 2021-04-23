85f9a7612f3e948c7207459c7c5e823b9c37a63b
RosarioMeyerArana <rosario.meyera@gmail.com>
2021-04-23 15:33:03 +0200

    navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import ItemListContainer from './Container/ItemListContainer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-light">
       <NavBar/>
       <ItemListContainer/>
     </div>
--- my-app/src/Components/NavBar/NavBar.js
+++ my-app/src/Components/NavBar/NavBar.js
@@ -2,27 +2,25 @@ import React from 'react';
 import CartWidget from '../CartWidget/CartWidget';
 import css from '../NavBar/NavBar.css';
 import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import logo from '../../Images/logo.png';
 
 const NavBar = () => {
 
     return(
         <header>
-        <nav className="navbar">
+            <Navbar bg="light" variant="light" className="shadow px-5">
             <div className="container-logo">
             <img className="logo" src={logo} alt="" />
             </div>
-
-            <ul className="navbar-item-container">
-                <li className="navbar-item">Nosotros</li>
-                <li className="navbar-item">Bodegas</li>
-                <li className="navbar-item">Contacto</li>
-                <li className="navbar-item">Shop</li>
-            </ul>
-
+            <Nav className="mr-auto">
+                <Nav.Link href="#home" className="navbar-item">Nosotros</Nav.Link>
+                <Nav.Link href="#features" className="navbar-item" >Contacto</Nav.Link>
+                <Nav.Link href="#pricing" className="navbar-item" >Shop</Nav.Link>
+            </Nav>
             <CartWidget/>
-
-        </nav>
+            </Navbar>
         </header>
     )
     
